5b38ccb487ea0949fe9aad964905ba83285f8d9f
mohamed <msalad@uncc.edu>
2017-03-15 01:18:47 -0400

    Init

---
+++ NBA_Simulator.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let teams = 10;
+let players = 30;
+
+
+
+
+
+
+
+
+
+
+public class Player {
+    
+    
+    init(){
+        
+    }
+    var team :String;
+    var name :String;
+    var position :String;
+    
+    var points :Int;
+    var assists :Int;
+    var rebounds :Int;
+    var blocks :Int;
+    var steals :Int;
+    
+    var fgMade :Int;
+    var ftMade :Int;
+    var tMade :Int;
+    var fgMissed :Int;
+    var ftMissed :Int;
+    var tMissed :Int;
+    
+    var fgp :Decimal;
+    var ftp :Decimal;
+    var tp :Decimal;
+    
+    var ranking :Int;
+
+    
+    
+}
